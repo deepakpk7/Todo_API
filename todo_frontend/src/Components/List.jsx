5ec46b81a801444655824f358e3dcbca30a8fe57
deepakpk7 <deepakpk7034@gmail.com>
2025-01-08 13:26:50 +0530

    updated

--- todo_frontend/src/Components/List.jsx
+++ todo_frontend/src/Components/List.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 function List(){
     const [data,setData] = useState([])
+    const [editing,setEditing] = useState(false)
+    const [editdata,setEditData] = useState(null)
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/api/todo/').then((res)=>{
             console.log(res.data);
@@ -10,6 +12,10 @@ function List(){
         }).catch(error=>console.log(error.message))
         
     },[])
+    const Edit_dtls = (task) =>{
+        setEditing(true)
+        setEditData(task)
+    }
     return(
         <div className="container">
             <h1>Display Details</h1>
@@ -25,15 +31,25 @@ function List(){
                         <tr key={index}>
                             <td>{value.task}</td>
                             <td>{value.description}</td>
-                            <td><button>Edit</button></td>
-                            <td><button>Delete</button></td>
+                            <td><button className="btn btn-outline-info" onClick={()=>{Edit_dtls(value)}}>Edit</button></td>
+                            <td><button className="btn btn-outline-danger">Delete</button></td>
 
                         </tr>
                     ))}
                 </tbody>
             </table>
-
+            {editing ? <EditForm curTask={editdata}/>:null}
         </div>
+    )
+}
+const EditForm = ({curTask})=>{
+    const [task,setTask] = useState(curTask)
+    return (
+        <form>
+            <input type="text" name="title" id="title" value={task.task}/>
+            <input type="text" name="description" id="description" value={task.description}/>
+            <input type="submit" value="Update" />
+        </form>
     )
 }
 export default List
